27ee003c77b46f8ea3961e2b91ad38dba96d42ca
Adhenrique12 <adeliofonseca.lord@hotmail.com>
2023-03-19 15:11:21 -0100

    delete my_plugin

--- handlers/tools/soup.py
+++ handlers/tools/soup.py
@@ -8,32 +8,8 @@ def soup(site_link: str) -> str:
     """This funtion return a link of the video."""
 
     link_video = site_link
-
-    motherless = link_video[8:18]  # https://motherless.com/
-    hotleak = link_video[8:15]  # https://hotleak.vip/
-    links = [motherless, hotleak]
-
-    result = requests.get(link_video)
-    soup = BeautifulSoup(result.text, "html.parser")
+    #link_video =  my_sites.link_video(site_link)
 
     print(link_video)
-
-    for link in links:
-        match link:
-            case "motherless":
-                links = soup.find_all("source")
-                link_video_to_download = links[0].get("src")
-                return link_video_to_download
-
-            case "hotleak":
-                links = soup.find_all("div", {"class": "light-gallery-item"})[0][
-                    "data-video"
-                ]
-                link = json.loads(links)
-                return link["source"][0]["src"]
-
-    m = re.search(r"https:\/\/.*\.mp4", result.text)
-    if m is not None and link_video[8:17] != "spankbang":
-        return m.group(0)
 
     return link_video
